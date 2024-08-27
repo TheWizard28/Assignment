dba1f82213ba48ef055ab4adf0bdc688e19ef2fa
Makoto Po <mr.cool2895@gmail.com>
2024-08-27 11:19:15 -0400

    add TylerPonce1A.py

---
+++ TylerPonce1A.py
@@ -0,0 +1,22 @@
+Course = 'Movie tickets for Sale'
+Maximum number of tickets = 20
+
+n = 20
+x = 1, 2,3,4
+
+n = n - x
+
+n = int(input("Enter number of tickets: "))
+
+if tickets <4:
+	print("Sorry but the max number of tickets per person is 4")
+
+elif tickets >4:
+	print("Thank you for your selection, Please enjoy your movie")
+
+sum = n - x
+
+loop(sum = n - x)
+
+if n = 0:
+	print('Sorry but all tickets have been sold')
