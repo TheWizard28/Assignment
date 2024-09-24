fb07a23efeace9fe8f1e756c56937aeea0cc4adf
TheWizard28 <mr.cool2895@gmail.com>
2024-09-24 10:12:41 -0400

    Add files via upload

--- TylerPonce2A.py
+++ TylerPonce2A.py
@@ -1,30 +1,31 @@
 #This program will scan emails to detect for spam risk
 #This will be the list of keywords to look for
-def is_spam_phrases_index(spam_words):
-    spam_words = ['act now','urgent','free','earn money','click here','buy now','limited time offer','get rich quick','make money now','earn extra cash','guaranteed',
+def check_spam():
+    #This is the spam list to reference
+    spam_words = ['act now', 'urgent', 'free', 'earn money', 'click here', 'buy now', 'limited time offer', 'get rich quick', 'make money now', 'earn extra cash', 'guaranteed',
                   'winner','bonus','winner','act now','while supplies last','risk free','great offer','click bellow','click here,','drastically reduced','sign up free today',
-                  'order today','apply now','special promotion','fantastic deal','satisfaction guaranteed','free quote','no strings attached','cards accepted',]
+                  'order today','apply now','special promotion','fantastic deal','satisfaction guaranteed','free quote','no strings attached','cards accepted']
 
-#These will be the variables for searching and counting the number of spam words
-spam_str = spam_words
-email = spam_str.count(spam_words)
-spam_num = -1
+    #This is where the user will input their email
+    email = input('Please enter your email')
 
-while True: #This is where the user will enter their email
-        question = input("Please enter your email (type 'n' to exit);")
-        spam_num = email_str.find(spam_str, spam_num + 1)
-        #This section will note the levels of risk in the email
-        if spam_num <5:
-            print('Low risk of Spam detected')
+    #This will be the counter
+    spam_num = 0
 
-        if spam_num  >10 <20:
-            print("Moderate risk of Spam detected")
+    for words in spam_words:
+        if words.lower() in email.lower():
+            spam_num += 1
 
-        if spam_num >20:
-            print('High risk of Spam detected')
+    if spam_num <= 5:
+        print('Low risk of Spam detected')
 
-        if question.lower() == 'n':
-            print('Thank you for your time')
-            break#Will allow user to exit program if they are finished
+    elif 5 < spam_num <= 20:
+        print("Moderate risk of Spam detected")
 
+    else:
+        print('High risk of Spam detected')
 
+    print(f'Your spam score is [spam_num]')
+    print('Thank you for your time')
+
+check_spam()
